297cc69ae3e9d04e9fc32854436ddd79882b3660
naichenzhao <naichenzhao13@gmail.com>
2025-04-02 20:37:11 -0700

    wuick commit before merge

--- studentControllerInterface.m
+++ studentControllerInterface.m
@@ -19,7 +19,7 @@ classdef studentControllerInterface < matlab.System
         V_servo = 0;
 
         x_hat = [-0.19; 0.00; 0; 0];
-        M = eye(2);
+        M = 0;
         W = 0;
         V = 0;
     end
@@ -74,28 +74,27 @@ classdef studentControllerInterface < matlab.System
             % Calculate kalman states
             syms x1 x2 x3 x4;
             A_lin = double(subs(obj.A, [x1, x2, x3, x4], obj.x_hat'));
-
+            
             L = -obj.M*obj.C'*obj.V;
             dx_hat = A_lin*obj.x_hat + obj.B*obj.V_servo + L * (y - obj.C*obj.x_hat);
-            obj.x_hat = obj.x_hat + dx_hat*dt;
+            % obj.x_hat = obj.x_hat + dx_hat*dt;
+
+            obj.x_hat = [p_ball, dx_hat(1), theta, dx_hat(3)]';
+            
 
             dM = A_lin*obj.M + obj.M*A_lin' + obj.W - obj.M*obj.C'*obj.V*obj.C*obj.M;
             obj.M = obj.M + dM*dt;
-            
             x = obj.x_hat';
-            obj.t_prev = t;
 
         end
 
         function x = generic_SE(obj, t, p_ball, theta)
+            dt = t - obj.t_prev;
 
-            p_vel = (p_ball-obj.p_prev)/(t-obj.t_prev);
-            theta_vel = (theta-obj.theta_prev)/(t-obj.t_prev);
+            p_vel = (p_ball-obj.p_prev)/dt;
+            theta_vel = (theta-obj.theta_prev)/dt;
 
             x = [p_ball, p_vel, theta, theta_vel];
-
-            obj.p_prev = p_ball;
-            obj.theta_prev = theta;
 
         end
         
@@ -115,8 +114,9 @@ classdef studentControllerInterface < matlab.System
             
             % fetch the previous values
             [p_ball_ref, v_ball_ref, a_ball_ref] = get_ref_traj(t);
-
             x = x - [p_ball_ref, v_ball_ref, 0, 0];
+
+            
             syms x1 x2 x3 x4;
             A_lin = double(subs(obj.A, [x1, x2, x3, x4], x));
             K = lqr(A_lin, obj.B, obj.Q, obj.R);
@@ -134,9 +134,13 @@ classdef studentControllerInterface < matlab.System
             
             xg = generic_SE(obj, t, p_ball, theta);
             xk = kalmanFilter(obj, t, p_ball, theta);
-            disp(xg - xk)
-            V_servo = stepImplLQR(obj, t, xk);
+            V_servo = stepImplLQR(obj, t, xg);
             theta_d = obj.theta_d;
+
+
+            obj.t_prev = t;
+            obj.p_prev = p_ball;
+            obj.theta_prev = theta;
         end
 
 
@@ -166,16 +170,13 @@ classdef studentControllerInterface < matlab.System
 
 
             obj.x_hat = [-0.19; 0.00; 0; 0];
-            obj.M = [1, 0, 0, 0;
-                     0, 1, 0, 0; 
-                     0, 0, 1, 0; 
-                     0, 0, 0, 1];
+            obj.M = eye(4);
 
-            obj.W = [1, 0, 0, 0;
+            obj.W = 0.01 * [1, 0, 0, 0;
                      0, 1, 0, 0; 
                      0, 0, 1, 0; 
                      0, 0, 0, 1];
-            obj.V = eye(2);
+            obj.V = 0.01 * eye(2);
         end
 
     end
